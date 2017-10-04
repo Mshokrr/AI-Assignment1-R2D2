3e5c8c112f73f52041ceba56e6f5d26b14cabe7f
mshokrr <mohamedshokrr@gmail.com>
2017-10-04 17:09:30 +0200

    console logs and adding rocks

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -9,6 +9,10 @@ public class Grid {
 	public Grid(int width, int height, int numberOfPads, int numberOfObstacles){
 		this.width = width;
 		this.height = height;
+		System.out.println("WELCOME\n");
+		System.out.println("Escape mission of R2D2 from the Death Star");
+		System.out.println("========\n\n");
+		System.out.println("Initialization Log:\n");
 		if (width < 5){
 			System.out.println("=> Width should be a minimum of 5, auto-initializing to 5");
 			this.width = 5;
@@ -33,12 +37,31 @@ public class Grid {
 		while ((this.width * height) < (numberOfObstacles / 4)){
 			numberOfObstacles /= 2;
 		}
-		for (int i = 0; i < numberOfPads; i++){
-			this.cells[(int) Math.floor(Math.random()*this.width)][(int) Math.floor(Math.random()*this.height)].status = CellStatus.pressurePad;
-		}
+		
 		for (int i = 0; i < numberOfObstacles; i++){
-			this.cells[(int) Math.floor(Math.random()*this.width)][(int) Math.floor(Math.random()*this.height)].status = CellStatus.obstacle;
+			int obstaclePositionX = (int) Math.floor(Math.random()*this.width);
+			int obstaclePositionY = (int) Math.floor(Math.random()*this.height);
+			System.out.println("=> Obstacle at: R" + obstaclePositionX + " C" + obstaclePositionY);
+			this.cells[obstaclePositionX][obstaclePositionY].status = CellStatus.obstacle;
 		}
+		
+		for (int i = 0; i < numberOfPads; i++){
+			int padPositionX = (int) Math.floor(Math.random()*this.width);
+			int padPositionY = (int) Math.floor(Math.random()*this.height);
+			System.out.println("=> Pad at: R" + padPositionX + " C" + padPositionY);
+			this.cells[padPositionX][padPositionY].status = CellStatus.pressurePad;
+			int rockPositionX = (int) Math.floor(Math.random()*this.width);
+			int rockPositionY = (int) Math.floor(Math.random()*this.height);
+			while (this.cells[rockPositionX][rockPositionY].status == CellStatus.pressurePad){
+				rockPositionX = (int) Math.floor(Math.random()*this.width);
+				rockPositionY = (int) Math.floor(Math.random()*this.height);
+			}
+			System.out.println("=> Rock at: R" + rockPositionX + " C" + rockPositionY);
+			this.cells[rockPositionX][rockPositionY].hasRock = true;
+		}
+		
+		System.out.println("========\n\nThe Grid:\n");
+		this.displayGrid();
 	}
 	
 	public void displayGrid(){
@@ -47,9 +70,17 @@ public class Grid {
 			for (int j = 0; j < this.height; j++){
 				if (this.cells[i][j].status == CellStatus.obstacle)
 					System.out.print("|  o  ");
-				else if (this.cells[i][j].status == CellStatus.pressurePad)
-					System.out.print("|  p  ");
-				else System.out.print("|     ");
+				else {	
+					if (this.cells[i][j].status == CellStatus.pressurePad)
+						System.out.print("|  p  ");
+				
+				else {
+					if (this.cells[i][j].hasRock)
+						System.out.print("|  r  ");
+					else 
+					System.out.print("|     ");
+					}
+				}
 			}
 			System.out.println();
 			for (int k = 0; k < this.width; k++){
@@ -62,20 +93,7 @@ public class Grid {
 	public static void main(String[] args) {
 		
 		Grid g = new Grid(4,4,6,6);
-		g.displayGrid();
-		
-//		Trying to create a grid in console
-//		for (int i = 0; i < 5; i++){
-//			System.out.println();
-//			for (int j = 0; j < 6; j++){
-//				System.out.print("|  b  ");
-//			}
-//			System.out.println();
-//			for (int k = 0; k < 6; k++){
-//				System.out.print("   _  ");
-//			}
-//			System.out.println();
-//		}
+
 	}
 
 }
