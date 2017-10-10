67b3c3cfdd7b5011b91e295f2fa9629990942912
Hazem El Agaty <hazemelagaty@Hazems-MacBook-Pro.local>
2017-10-10 20:08:57 +0200

    change state teleport position and add it to problem

--- src/Assignment1/MyState.java
+++ src/Assignment1/MyState.java
@@ -4,15 +4,13 @@ import Grid.Cell;
 import Search.State;
 
 public class MyState extends State {
-	
+
 	Cell currentPosition;
 	int unactivatedPads;
-	Cell telePosition; //for now.
-	
+
 	public MyState(Cell currentPosition, int unactivatedPads, Cell telePosition) {
 		this.currentPosition = currentPosition;
 		this.unactivatedPads = unactivatedPads;
-		this.telePosition = telePosition;
 	}
-	
+
 }
