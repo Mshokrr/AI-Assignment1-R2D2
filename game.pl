8adff4997c56c203734f5970b54c284fd7cbce10
Youssuf Radi <y.sameh1995@gmail.com>
2017-11-13 23:24:12 +0200

    Update game.pl

--- game.pl
+++ game.pl
@@ -1,63 +1,52 @@
-pad(3,1).
+pad(2,0).
 
-teleport(1,1).
-obstacle(2,0).
+teleport(0,0).
+obstacle(0,2).
 
 w(0).
 w(1).
 w(2).
-w(3).
+
 h(0).
 h(1).
 h(2).
-h(3).
 
-at(I,J,S):-
-  agent_helper(I,J,S,1).
+run(Q):-
+  run_helper(Q,3).
 
-not_inst(Var):-
-  \+(\+(Var=0)),
-  \+(\+(Var=1)).
+run_helper(Q, I):-
+  call_with_depth_limit(Q,I,R),
+  run_helper2(Q,I,R).
+  
+run_helper2(_, _, R):-
+  R \= depth_limit_exceeded.
 
-agent_helper(I,J,S,M):-
-  \+not_inst(S),
-  agent(I,J,S,M).
+run_helper2(Q, I, R):-
+  R == depth_limit_exceeded,
+  I1 is I +1,
+  run_helper(Q,I1).
 
-agent_helper(I,J,S,M):-
-  not_inst(S),
-  agent(I,J,S,M),
-  N is M+1,
-  agent_helper(I,J,S,N).
+agent(2,2,s0).
 
-agent(0,3,s0,_).
-
-agent(I, J, result(down,S), M):-
-  M > 0,
+agent(I, J, result(down,S)):-
   move_agent(I,J,0,-1, S),
   N is J-1,
-  M1 is M-1,
-  agent(I,N,S,M1).
+  agent(I,N,S).
 
-agent(I, J, result(up,S), M):-
-  M > 0,
+agent(I, J, result(up,S)):-
   move_agent(I,J,0,1,S),
   N is J+1,
-  M1 is M-1,
-  agent(I,N,S,M1).
+  agent(I,N,S).
 
-agent(I, J, result(right,S), M):-
-  M > 0,
+agent(I, J, result(right,S)):-
   move_agent(I,J,-1,0,S),
   N is I-1,
-  M1 is M-1,
-  agent(N,J,S,M1).
+  agent(N,J,S).
 
-agent(I, J, result(left,S), M):-
-  M > 0,
+agent(I, J, result(left,S)):-
   move_agent(I,J,1,0,S),
   N is I+1,
-  M1 is M-1,
-  agent(N,J,S,M1).
+  agent(N,J,S).
 
 move_agent(I, J, _, _, S):-
   \+ rock(I,J,S),
@@ -72,38 +61,52 @@ move_agent(I, J, K, L, S):-
   \+pad(I,J),
   \+rock(N1,N2, S),
   \+obstacle(N1,N2),
-  \+w(N1),
-  \+h(N2).
+  w(N1),
+  h(N2).
 
-rock(0,1,s0).
+rock(2,1,s0).
 
-%%rock(I, J, result(down, S)):-
-  %%move_rock(I, J, 0, -2, S),
-  %%N is J-1,
-  %%rock(I,N,S).
+rock(I, J, result(down, S)):-
+  move_rock(I, J, 0, -1, S),
+  N is J-1,
+  rock(I,N,S).
 
-%%rock(I, J, result(up, S)):-
-  %%move_rock(I, J, 0, 2, S),
-  %%N is J+1,
-  %%rock(I,N,S).
+rock(I, J, result(up, S)):-
+  move_rock(I, J, 0, 1, S),
+  N is J+1,
+  rock(I,N,S).
 
-%%rock(I, J, result(right, S)):-
-  %%move_rock(I, J, -2, 0, S),
-  %%N is I-1,
-  %%rock(N,J,S).
+rock(I, J, result(right, S)):-
+  move_rock(I, J, -1, 0, S),
+  N is I-1,
+  rock(N,J,S).
 
-%%rock(I, J, result(left, S)):-
-%%  move_rock(I, J, 2, 0, S),
-%%  N is I+1,
-%%  rock(N,J,S).
+rock(I, J, result(left, S)):-
+  move_rock(I, J, 1, 0, S),
+  N is I+2,
+  rock(N,J,S).
 
-move_rock(I, J, K, L, S):-
-  N1 is I+K,
-  N2 is J+L,
-  agent(N1,N2,S,-1).
-
-move_rock(I, J, K, L, S):-
-  N1 is I+K,
-  N2 is J+L,
-  \+agent(N1,N2,S,-1),
+rock(I, J, result(_, S)):-
+  \+move_rock(I, J, 1, 0, S),
+  \+move_rock(I, J, 11, 0, S),
+  \+move_rock(I, J, 0, -1, S),
+  \+move_rock(I, J, 0, 1, S),
   rock(I,J,S).
+  
+move_rock(I, J, K, L, S):-
+  N1 is I+K+K,
+  N2 is J+L+L,
+  agent(N1,N2,S),
+  \+rock(I,J,S),
+  \+obstacle(I,J),
+  N3 is I+K,
+  N4 is J+L,
+  \+pad(N3,N4),
+  w(I),
+  h(J).
+
+%%move_rock(I, J, K, L, S):-
+  %%N1 is I-K,
+  %%N2 is J-L,
+  %%\+agent(N1,N2,S),
+  %%rock(I,J,S).
